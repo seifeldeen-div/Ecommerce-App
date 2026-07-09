53bb990fd5c474809ba672cae85b52655bb01941
Eng . Seif Eldeen <seifedeen112006@gmail.com>
2026-07-10 01:26:00 +0300

    Home

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.min.js"
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import Home from './pages/Home/Home'
 
 function App() {
   return (
     <>
       <Navbar />
+      <Home />
       <Footer />
     </>
   )
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 function Home() {
     return (
         <>
-            
+            <h1 className="bg-blue-900 text-white text-3xl p-5">ياك ننفض بقي بيقولكو سيف</h1>
         </>
     )
 }
